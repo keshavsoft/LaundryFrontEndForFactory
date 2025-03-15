1c11468ddf90cd12ba51d62b1b853862344adeee
sharath0036 <ksharathbabu2@gmail.com>
2025-03-15 13:44:04 +0530

    login for factory

--- FromBranch/FromBranchVoucher/Js/pages/StatusDCBranchFilter/StartFunc.js
+++ FromBranch/FromBranchVoucher/Js/pages/StatusDCBranchFilter/StartFunc.js
@@ -1,7 +1,9 @@
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
+import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 
 const StartFunc = () => {
-    let jVarLocalFromAdmin = true;
+    // let jVarLocalFromAdmin = true;
+    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncFormLoad });
 
     if (jVarLocalFromAdmin) {
         StartFuncFormLoad();
