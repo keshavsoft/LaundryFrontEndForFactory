dabe84a25c339933a7edf8e70da1b9b47f58fd7b
sharath0036 <ksharathbabu2@gmail.com>
2025-03-17 13:55:49 +0530

    login for washing and presing

--- AtFactory/PressingVoucher/Js/pages/ShowReturnDcQrs/StartFunc.js
+++ AtFactory/PressingVoucher/Js/pages/ShowReturnDcQrs/StartFunc.js
@@ -1,14 +1,12 @@
 import { StartFunc as StartFuncShowOnDom } from "./ShowOnDom.js";
 import { StartFunc as StartFuncFormLoad } from "./FormLoad/StartFunc.js";
 // import { StartFunc as StartFuncFormLoadBeforeAdmin } from "./FormLoadBeforeAdmin/EntryFile.js";
+import { StartFunc as LoginModal } from "../../../assets/static/js/Common/NiceAdmin/Header/LoginModal/EntryFile.js";
 
 const StartFunc = () => {
     StartFuncFormLoad();
-
-    // StartFuncFormLoadBeforeAdmin();
-
-    let jVarLocalFromAdmin = true;
-
+    let jVarLocalFromAdmin = LoginModal({ inSuccessFunc: StartFuncFormLoad });
+ 
     if (jVarLocalFromAdmin) {
         StartFuncShowOnDom({}).then();
     };
