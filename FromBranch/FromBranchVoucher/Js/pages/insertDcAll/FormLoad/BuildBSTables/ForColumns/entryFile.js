6a2ac09e6424f64d482441e00117e6dcc9bf0dd8
sharath0036 <ksharathbabu2@gmail.com>
2025-03-21 19:08:14 +0530

    from and to dates Filte && Dark mode

---
+++ FromBranch/FromBranchVoucher/Js/pages/insertDcAll/FormLoad/BuildBSTables/ForColumns/entryFile.js
@@ -0,0 +1,38 @@
+// import { StartFunc as ColumnOperate } from "./ColumnOperate/entryFile.js";
+import { StartFunc as SentFromBranch } from "./SentFromBranch/entryFile.js";
+import { StartFunc as Scanned } from "./Scanned/entryFile.js";
+// import { StartFunc as Pending } from "./Pending/entryFile.js";
+import { StartFunc as EntryCancel } from "./EntryCancel/entryFile.js";
+import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+
+
+
+let StartFunc = ({ inColumns }) => {
+    let LocalColumns = inColumns;
+    // let LocalColumnOperateFine = LocalColumns.find(element => element.field === "operate");
+
+    // ColumnOperate({ inFindColumn: LocalColumnOperateFine });
+
+    let LocalColumnSentFromBranch= LocalColumns.find(element => element.field === "Sent");
+
+    SentFromBranch({ inFindColumn: LocalColumnSentFromBranch });
+
+    let LocalColumnScanned= LocalColumns.find(element => element.field === "scanned");
+
+    Scanned({ inFindColumn: LocalColumnScanned });
+
+    // let LocalColumnPending= LocalColumns.find(element => element.field === "Pending");
+
+    // Pending({ inFindColumn: LocalColumnPending });
+
+    let LocalColumnEntryCancel= LocalColumns.find(element => element.field === "EntryCancel");
+
+    EntryCancel({ inFindColumn: LocalColumnEntryCancel });
+
+    let LocalColumnSerial= LocalColumns.find(element => element.field === "KS-Serial");
+
+    ColumnSerial({ inFindColumn: LocalColumnSerial });
+
+};
+
+export { StartFunc };
